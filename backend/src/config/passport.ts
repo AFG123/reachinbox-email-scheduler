09a4aaa1e89f8cf4181b3a0f2e21fd1a1d959875
AFG123 <aryan123damai@gmail.com>
2026-08-24 01:39:38 +0530

    fix: changed callbackURL to  dynamic using BACKEND_URL

--- backend/src/config/passport.ts
+++ backend/src/config/passport.ts
@@ -17,13 +17,15 @@ passport.deserializeUser(async (id: string, done) => {
   }
 });
 
+const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:5000';
+
 // Configure the Google Strategy
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-      callbackURL: 'http://localhost:5000/api/auth/google/callback',
+      callbackURL: `${BACKEND_URL}/api/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
